032d698462facdfb4f6dfd2c322860ef3ccb94e6
orixflo <orixflo@gmail.com>
2023-05-24 05:24:54 +0900

    Add Color palette

---
+++ online_omok-client/src/styles/colors.js
@@ -0,0 +1,87 @@
+export const colorConvex = [
+    `
+        // Internal frame
+        -2px -2px #DFDFDF,
+        -2px 0px #DFDFDF,
+        0px -2px #DFDFDF,
+        2px 2px #808080,
+        2px -2px #808080,
+        -2px 2px #808080,
+        // External frame
+        -4px -4px #FFFFFF,
+        -4px 2px #FFFFFF,
+        2px -4px #FFFFFF,
+        4px 4px #000000,
+        4px -4px #000000,
+        -4px 4px #000000;
+    `,
+    `
+        // Internal frame
+        -3px -3px #FFFFFF,
+        -3px 0px #FFFFFF,
+        0px -3px #FFFFFF,
+        3px 3px #808080,
+        3px -3px #808080,
+        -3px 3px #808080,
+        // External frame
+        -6px -6px #DFDFDF,
+        -6px 3px #DFDFDF,
+        3px -6px #DFDFDF,
+        6px 6px #000000,
+        6px -6px #000000,
+        -6px 6px #000000;
+    `
+];
+
+export const colorConcave = [
+    `
+        // Internal frame
+        -2px -2px #000000,
+        -2px 0px #000000,
+        0px -2px #000000,
+        2px 2px #DFDFDF,
+        2px -2px #DFDFDF,
+        -2px 2px #DFDFDF,
+        // External frame
+        -4px -4px #808080,
+        -4px 2px #808080,
+        2px -4px #808080,
+        4px 4px #FFFFFF,
+        4px -4px #FFFFFF,
+        -4px 4px #FFFFFF;
+    `
+];
+
+export const colorScrollBtnOff = `
+    // Internal frame
+    inset -2px -2px #000000,
+    inset -2px 0px #000000,
+    inset 0px -2px #000000,
+    inset 2px 2px #FFFFFF,
+    inset 2px -2px #FFFFFF,
+    inset -2px 2px #FFFFFF,
+    // External frame
+    inset -4px -4px #808080,
+    inset -4px 2px #808080,
+    inset 2px -4px #808080,
+    inset 4px 4px #DFDFDF,
+    inset 4px -4px #DFDFDF,
+    inset -4px 4px #DFDFDF;
+`;
+
+export const colorScrollBtnOn = `
+    // Internal frame
+    inset -2px -2px #FFFFFF,
+    inset -2px 0px #FFFFFF,
+    inset 0px -2px #FFFFFF,
+    inset 2px 2px #808080,
+    inset 2px -2px #808080,
+    inset -2px 2px #808080,
+    // External frame
+    inset -4px -4px #DFDFDF,
+    inset -4px 2px #DFDFDF,
+    inset 2px -4px #DFDFDF,
+    inset 4px 4px #000000,
+    inset 4px -4px #000000,
+    inset -4px 4px #000000;
+`;
